97cbcb957e1601d7c75fb2c0ecf8643243ab0551
Daniel Livingston <daniel.livingston@live.com>
2024-08-01 20:55:36 -0400

    Initial Commit

---
+++ VoiceMemoTranscription/VoiceMemoTranscriptionApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceMemoTranscriptionApp.swift
+//  VoiceMemoTranscription
+//
+//  Created by Daniel Livingston on 8/1/24.
+//
+
+import SwiftUI
+
+@main
+struct VoiceMemoTranscriptionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
